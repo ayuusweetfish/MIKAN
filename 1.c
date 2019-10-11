dffeaf71b9c561be747e1020fa9a40ef5ba189ee
Shiqing <shiqing-thu18@yandex.com>
2019-10-12 01:24:24 +0800

    :alarm_clock: Add timer

--- 1.c
+++ 1.c
@@ -7,18 +7,35 @@
 #define GPSET1      (volatile uint32_t *)(GPIO_BASE + 0x20)
 #define GPCLR1      (volatile uint32_t *)(GPIO_BASE + 0x2c)
 
-float COUNT = 30000;
-const uint32_t FLAG = 1 << 15;
-uint32_t ZERO;  // .bss
+#define SYSTMR_BASE 0x20003000
+
+#define SYSTMR_CS   (volatile uint32_t *)(SYSTMR_BASE + 0x00)
+#define SYSTMR_CLO  (volatile uint32_t *)(SYSTMR_BASE + 0x04)
+#define SYSTMR_C0   (volatile uint32_t *)(SYSTMR_BASE + 0x0c)
+#define SYSTMR_C1   (volatile uint32_t *)(SYSTMR_BASE + 0x10)
+
+#define DMB() __asm__ __volatile__ ("mcr p15, 0, %0, c7, c10, 5" : : "r" (0) : "memory")
+#define DSB() __asm__ __volatile__ ("mcr p15, 0, %0, c7, c10, 4" : : "r" (0) : "memory")
+
+void wait()
+{
+    DSB();
+    uint32_t val = *SYSTMR_CLO;
+    *SYSTMR_C0 = val + 1000000;
+    while ((*SYSTMR_CS) & 1) { *SYSTMR_CS = 1; DMB(); DSB(); }
+    while (((*SYSTMR_CS) & 1) == 0) { }
+    while ((*SYSTMR_CS) & 1) { *SYSTMR_CS = 1; DMB(); DSB(); }
+    DMB();
+}
 
 void kernel_main()
 {
     *GPFSEL4 |= (1 << 21);
 
     while (1) {
-        *GPCLR1 = FLAG;
-        for (float t = ZERO; t < COUNT; t += 0.01f) __asm__("");
-        *GPSET1 = FLAG;
-        for (float t = ZERO; t < COUNT; t += 0.01f) __asm__("");
+        *GPCLR1 = (1 << 15);
+        wait();
+        *GPSET1 = (1 << 15);
+        wait();
     }
 }
