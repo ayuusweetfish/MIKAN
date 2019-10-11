f7eff498a66ab8179c645206518bb1398776150b
Shiqing <shiqing-thu18@yandex.com>
2019-10-11 11:23:03 +0800

    :dizzy: New way for unoptimized loops

--- 1.c
+++ 1.c
@@ -7,8 +7,6 @@
 #define GPSET1      (volatile uint32_t *)(GPIO_BASE + 0x20)
 #define GPCLR1      (volatile uint32_t *)(GPIO_BASE + 0x2c)
 
-volatile uint32_t t;
-
 int main() __attribute((naked));
 int main()
 {
@@ -16,8 +14,8 @@ int main()
 
     while (1) {
         *GPCLR1 = (1 << 15);
-        for (t = 0; t < 1500000; t++) { }
+        for (uint32_t t = 0; t < 5000000; t++) __asm__("");
         *GPSET1 = (1 << 15);
-        for (t = 0; t < 1500000; t++) { }
+        for (uint32_t t = 0; t < 5000000; t++) __asm__("");
     }
 }
