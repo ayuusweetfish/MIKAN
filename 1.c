684d296c0b4c2dd165c062f6acff8d772bd9103c
Shiqing <shiqing-thu18@yandex.com>
2019-10-11 19:36:48 +0800

    :link: Add link script

--- 1.c
+++ 1.c
@@ -7,6 +7,8 @@
 #define GPSET1      (volatile uint32_t *)(GPIO_BASE + 0x20)
 #define GPCLR1      (volatile uint32_t *)(GPIO_BASE + 0x2c)
 
+uint32_t COUNT = 3000000;
+
 int main() __attribute((naked));
 int main()
 {
@@ -14,8 +16,8 @@ int main()
 
     while (1) {
         *GPCLR1 = (1 << 15);
-        for (uint32_t t = 0; t < 5000000; t++) __asm__("");
+        for (uint32_t t = 0; t < COUNT; t++) __asm__("");
         *GPSET1 = (1 << 15);
-        for (uint32_t t = 0; t < 5000000; t++) __asm__("");
+        for (uint32_t t = 0; t < COUNT; t++) __asm__("");
     }
 }
