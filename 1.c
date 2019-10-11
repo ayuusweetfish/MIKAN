2c3b3febe8e7a7026747935886466e8630605ffc
Shiqing <shiqing-thu18@yandex.com>
2019-10-11 19:50:44 +0800

    :package: Add stack pointer

--- 1.c
+++ 1.c
@@ -8,16 +8,22 @@
 #define GPCLR1      (volatile uint32_t *)(GPIO_BASE + 0x2c)
 
 uint32_t COUNT = 3000000;
+const uint32_t FLAG = 1 << 15;
 
-int main() __attribute((naked));
-int main()
+uint32_t flag() __attribute__((noinline, optimize(0)));
+uint32_t flag() { return FLAG; }
+
+void on() __attribute__((noinline));
+void on() { *GPCLR1 = flag(); }
+
+int kernel_main()
 {
     *GPFSEL4 |= (1 << 21);
 
     while (1) {
-        *GPCLR1 = (1 << 15);
+        on();
         for (uint32_t t = 0; t < COUNT; t++) __asm__("");
-        *GPSET1 = (1 << 15);
+        *GPSET1 = FLAG;
         for (uint32_t t = 0; t < COUNT; t++) __asm__("");
     }
 }
