bb7d34cf79f9ae73f7c4aacce1e9840ad32a4521
Shiqing <shiqing-thu18@yandex.com>
2019-10-21 22:37:37 +0800

    :joystick: Add gamepad driver template

--- build_csud.sh
+++ build_csud.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd csud
 make clean
-CFLAGS="-mfpu=vfp -mfloat-abi=hard -march=armv6k -mtune=arm1176jzf-s" make driver CONFIG=DEBUG TYPE=LOWLEVEL TARGET=RPI GNU=arm-none-eabi- LIB_KBD=1 LIB_MOUSE=0
+CFLAGS="-mfpu=vfp -mfloat-abi=hard -march=armv6k -mtune=arm1176jzf-s" make driver CONFIG=DEBUG TYPE=LOWLEVEL TARGET=RPI GNU=arm-none-eabi- LIB_KBD=1 LIB_MOUSE=0 LIB_GAMEPAD=1
