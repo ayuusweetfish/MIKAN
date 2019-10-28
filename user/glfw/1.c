8ae8aae64948d0f6473e635e1b83e0da06e252c9
Shiqing <shiqing-thu18@yandex.com>
2019-10-28 22:22:49 +0800

    :turtle: Inertia

--- user/glfw/1.c
+++ user/glfw/1.c
@@ -235,25 +235,32 @@ inline uint32_t buttons()
 #ifndef M_PI
 #define M_PI  3.1415926535897932384626433832795
 #endif
+#ifndef M_SQRT1_2
+#define M_SQRT1_2   0.7071067811865476
+#endif
 
+float p0[2];
+float v0[2];
 static int p[5][2];
 static int T = 0;
 
 static inline void recal_p()
 {
     float phase = T * M_PI / 240;
-    float r = 32 * (1.5 + sinf(phase * 1.96));
+    float r = 32 * (1.5f + sinf(phase * 1.96f));
     float x = r * cosf(phase), y = r * sinf(phase);
-    p[1][0] = p[0][0] + x; p[1][1] = p[0][1] + y;
-    p[2][0] = p[0][0] - y; p[2][1] = p[0][1] + x;
-    p[3][0] = p[0][0] - x; p[3][1] = p[0][1] - y;
-    p[4][0] = p[0][0] + y; p[4][1] = p[0][1] - x;
+    p[0][0] = (int)(p0[0] + 0.5f);
+    p[0][1] = (int)(p0[1] + 0.5f);
+    p[1][0] = (int)(p0[0] + x); p[1][1] = (int)(p0[1] + y);
+    p[2][0] = (int)(p0[0] - y); p[2][1] = (int)(p0[1] + x);
+    p[3][0] = (int)(p0[0] - x); p[3][1] = (int)(p0[1] - y);
+    p[4][0] = (int)(p0[0] + y); p[4][1] = (int)(p0[1] - x);
 }
 
 void init()
 {
+    p0[0] = p0[1] = 128;
     puts("^ ^  Hello, world!");
-    p[0][0] = p[0][1] = 128;
 }
 
 void update()
@@ -261,17 +268,32 @@ void update()
     T++;
 
     uint32_t b = buttons();
-    if (b & BUTTON_UP) p[0][1] += 1;
-    if (b & BUTTON_DOWN) p[0][1] -= 1;
-    if (b & BUTTON_LEFT) p[0][0] -= 1;
-    if (b & BUTTON_RIGHT) p[0][0] += 1;
+    float v1[2] = { 0, 0 };
+    if (b & BUTTON_UP) v1[1] += 1;
+    if (b & BUTTON_DOWN) v1[1] -= 1;
+    if (b & BUTTON_LEFT) v1[0] -= 1;
+    if (b & BUTTON_RIGHT) v1[0] += 1;
+    if (v1[0] != 0 && v1[1] != 0) {
+        v1[0] *= M_SQRT1_2;
+        v1[1] *= M_SQRT1_2;
+    }
+
+    float v0len = v0[0] * v0[0] + v0[1] * v0[1];
+    float v1len = v1[0] * v1[0] + v1[1] * v1[1];
+    float delta[2] = { v1[0] - v0[0], v1[1] - v0[1] };
+    float rate = (v1len > v0len ? 0.1 : 0.05);
+    v0[0] += delta[0] * rate;
+    v0[1] += delta[1] * rate;
+    p0[0] += v0[0];
+    p0[1] += v0[1];
+
     recal_p();
 
     int x0, y0;
     for (int i = 0; i <= 4; i++)
     for (int x = -8; x <= 8; x++) if ((x0 = p[i][0] + x) >= 0 && x0 < 256)
     for (int y = 8; y >= -8; y--) if ((y0 = p[i][1] + y) >= 0 && y0 < 256) {
-        if (x * x + y * y > (int)(8.5 * 8.5)) continue;
+        if (x * x + y * y > (int)(8.5f * 8.5f)) continue;
         if (y0 > 0) pix(x0, y0 - 1, 128, 96, 32);
         if (x0 < 255) pix(x0 + 1, y0, 128, 96, 32);
         pix(x0, y0, 240, 192, 108);
